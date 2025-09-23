b8c472943b439940264747c72a7def3558aa61a0
Nnamdi Agu <nagu1@myseneca.ca>
2025-09-23 15:52:44 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Author : Nnamdi Agu
+
+
+#Author ID : nagu1
+
+
+#Date Created : 2025/09/23
+
+
+timer = 10
+
+while timer !=0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
+
